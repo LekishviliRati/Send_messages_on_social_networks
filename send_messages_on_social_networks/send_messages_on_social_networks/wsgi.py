b3b32ebdc0df7008ceda43963f6de8e9e8594209
LekishviliRati <lekishvili.rati@gmail.com>
2022-06-26 15:38:40 +0200

    Initialized django application

---
+++ send_messages_on_social_networks/send_messages_on_social_networks/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for send_messages_on_social_networks project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'send_messages_on_social_networks.settings')
+
+application = get_wsgi_application()
