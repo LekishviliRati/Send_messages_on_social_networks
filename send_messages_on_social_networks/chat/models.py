4afb751ab688bfabc5da3b851b7e05b7ecd37a88
LekishviliRati <lekishvili.rati@gmail.com>
2022-06-26 17:50:26 +0200

    Application connected with DB

--- send_messages_on_social_networks/chat/models.py
+++ send_messages_on_social_networks/chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    content = models.TextField(default="aucun messsage")
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+
