dfbc320d0e3d982bee431df0c7fbcea34b9e12b8
LekishviliRati <lekishvili.rati@gmail.com>
2022-06-27 09:37:51 +0200

    Templates display Data Base content, all is working good !

--- send_messages_on_social_networks/chat/views.py
+++ send_messages_on_social_networks/chat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Message
 
-# Create your views here.
+
+def list_message(request):
+    message = Message.objects.all()
+    return render(request, 'list_messages.html', {'messages': message})
