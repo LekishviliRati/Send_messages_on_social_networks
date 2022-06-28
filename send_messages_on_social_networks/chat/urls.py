a075a88aa85a27f406a38d91223c87727458d148
LekishviliRati <lekishvili.rati@gmail.com>
2022-06-28 08:52:58 +0200

    Improved front end

--- send_messages_on_social_networks/chat/urls.py
+++ send_messages_on_social_networks/chat/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from chat import views
 
 urlpatterns = [
-    path('', views.list_message, name="list_message"),
+    path('', views.home, name="home_page"),
+    path('messages/', views.list_message, name="list_message"),
 ]
--- send_messages_on_social_networks/chat/views.py
+++ send_messages_on_social_networks/chat/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Message
 
 
+def home(request):
+    return render(request, 'home.html')
+
+
 def list_message(request):
     message = Message.objects.all()
     return render(request, 'list_messages.html', {'messages': message})
