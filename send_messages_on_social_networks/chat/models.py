48b702d3ad1376a0dbeee4505136731d83eba803
LekishviliRati <lekishvili.rati@gmail.com>
2022-06-28 11:08:57 +0200

    Users are linked to messages

--- send_messages_on_social_networks/chat/models.py
+++ send_messages_on_social_networks/chat/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from users.models import User
 
 
 class Message(models.Model):
     content = models.TextField(default="aucun messsage")
     publication_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
 
 
