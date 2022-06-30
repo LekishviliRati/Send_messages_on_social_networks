40c554a41ca9a71597f9698564e615f338c0f471
LekishviliRati <lekishvili.rati@gmail.com>
2022-06-30 11:08:01 +0200

    Users can ONLY see their messages

--- send_messages_on_social_networks/chat/views.py
+++ send_messages_on_social_networks/chat/views.py
@@ -1,6 +1,4 @@
-import json
-
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Message
 from .forms import MessageRegisterForm
 from .discord_chat import SendMessageToDiscord
@@ -16,12 +14,13 @@ def list_message(request):
         return render(request, 'list_messages.html', locals())
     elif request.method == "POST":
         form = MessageRegisterForm(request.POST)
-        message = Message.objects.all()
         if form.is_valid():
             content = form.cleaned_data['content']
             send_content_to_discord = SendMessageToDiscord(content)
+            form.instance.author = request.user
             form.save()
             form = MessageRegisterForm()
+            user = request.user.id
+            message = Message.objects.filter(author_id=user)
             return render(request, 'list_messages.html', {'messages': message, 'form': form})
-            # return redirect("list_message")
 
--- send_messages_on_social_networks/users/forms.py
+++ send_messages_on_social_networks/users/forms.py
@@ -1,7 +1,7 @@
 from .models import User
 # from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import ModelForm
+# from django.forms import ModelForm
 
 
 class UserRegisterForm(UserCreationForm):
