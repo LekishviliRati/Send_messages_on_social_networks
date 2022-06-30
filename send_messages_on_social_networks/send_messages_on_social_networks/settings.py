98a68dd1238f4e412338c6327c795bfef3de5a3d
LekishviliRati <lekishvili.rati@gmail.com>
2022-06-30 13:50:05 +0200

    Updated Static root for Heroku

--- send_messages_on_social_networks/send_messages_on_social_networks/settings.py
+++ send_messages_on_social_networks/send_messages_on_social_networks/settings.py
@@ -135,6 +135,9 @@ USE_TZ = True
 
 STATIC_URL = 'chat/static/'
 
+# Heroku static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
